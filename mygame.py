19d1a49eb42d6397430d19460a44a770c70a9335
amonulloh <akhmedov.akh@phystech.edu>
2022-03-29 12:40:44 +0300

    Changed 4 files. Added class realisation.

--- mygame.py
+++ mygame.py
@@ -1,26 +1,17 @@
-#CS_2D
+#dCS_2D
 
 import pygame
+import window
+
+import player_1
+
+from player_1 import Player_1, Player_2
 
 pygame.init()
 
-window = pygame.display.set_mode((1000, 500))
+window.window
 
 pygame.display.set_caption("CS_2D")
-
-
-x_a = 940
-y_a = 440 
-width_a = 35
-height_a = 35
-speed_a = 5
-radius = 15
-
-x_b =60
-y_b = 60
-width_b = 35
-height_b = 35 
-speed_b = 5
 
 play = True
 
@@ -31,61 +22,8 @@ while play:
         if event.type == pygame.QUIT:
             play = False
 
-    move_a = pygame.key.get_pressed()
-    if move_a[pygame.K_LEFT] and x_a > 505 + radius:
-        x_a -= speed_a
-    if move_a[pygame.K_RIGHT] and x_a < 995 - radius:
-        x_a += speed_a
-    if move_a[pygame.K_UP] and y_a > radius:
-        y_a -= speed_a
-    if move_a[pygame.K_DOWN] and y_a < 495 - radius:
-        y_a += speed_a
-
+    player_1.pygame
+    Player_1
+    Player_2
     
-
-    window.fill((0, 0, 0))
-    pygame.draw.circle(window, (0, 0, 225), (x_a, y_a), 15)
-
-    move_b = pygame.key.get_pressed()
-    if move_b[pygame.K_a] and x_b > radius:
-        x_b -= speed_b
-
-    move_b = pygame.key.get_pressed()
-    if move_b[pygame.K_d] and x_b < 495 - radius:
-        x_b += speed_b
-
-    move_b = pygame.key.get_pressed()
-    if move_b[pygame.K_w] and y_b > radius:
-        y_b -= speed_b
-
-    move_b = pygame.key.get_pressed()
-    if move_b[pygame.K_s] and y_b < 495 - radius:
-        y_b += speed_b
-    
-
-    pygame.draw.circle(window, (255, 0, 0), (x_b, y_b), 15)
     pygame.display.update()
-
-
-""""
-jump = False
-jumpHeight = 10
-
-if not(jump):fcqefwef
-else:
-        
-        if jumpHeight >= -10:
-            if jumpHeight < 0:
-                y_a += (jumpHeight**2) / 2
-            else:    
-                y_a -= (jumpHeight**2) / 2
-            jumpHeight -= 1
-        else:
-            jump = False
-            jumpHeight = 10
-"""
-
-#classes:
-#   1. window;
-#   2. player_1;
-#   3. player_2;
--- player_1.py
+++ player_1.py
@@ -1,1 +1,43 @@
 #This Class for Player_1
+import pygame
+import window
+
+x_a = window.Window.x_a
+x_b = window.Window.x_b
+y_a = window.Window.y_a
+y_b = window.Window.y_b
+radius = window.Window.radius
+speed = window.Window.speed
+
+from window import window
+
+class Player_1():
+    move_a = pygame.key.get_pressed()
+    if move_a[pygame.K_LEFT] and x_a > 505 + radius:
+        x_a -= speed
+    if move_a[pygame.K_RIGHT] and x_a < 995 - radius:
+        x_a += speed
+    if move_a[pygame.K_UP] and y_a > radius:
+        y_a -= speed
+    if move_a[pygame.K_DOWN] and y_a < 495 - radius:
+        y_a += speed
+
+    pygame.draw.circle(window, (0, 0, 225), (x_a, y_a), 15)
+    pass
+class Player_2():
+    move_b = pygame.key.get_pressed()
+    if move_b[pygame.K_a] and x_b > radius:
+        x_b -= speed
+
+    if move_b[pygame.K_d] and x_b < 495 - radius:
+        x_b += speed
+
+    if move_b[pygame.K_w] and y_b > radius:
+        y_b -= speed
+
+    if move_b[pygame.K_s] and y_b < 495 - radius:
+        y_b += speed
+
+    pygame.draw.circle(window, (255, 0, 0), (x_b, y_b), 15)
+    
+    pass
