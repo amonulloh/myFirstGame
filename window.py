bc092f4fc74a8dee7c8397c7eba0648c4f545057
amonulloh <akhmedov.akh@phystech.edu>
2022-04-10 22:55:26 +0300

    Changes added

---
+++ window.py
@@ -0,0 +1,15 @@
+#This Class for Window
+
+import pygame
+
+length = 1000
+width = 700
+windows = pygame.display.set_mode((length, width))
+
+class Window():
+    pass
+
+class Play():
+    windows.fill((0, 0, 0))
+    pygame.display.update()
+    pass
